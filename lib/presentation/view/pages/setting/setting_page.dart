1a170b27a34bc8b1228dd28886f3dea4b19f4e96
Trinh Quang Lam <trinhquanglam2k4@gmail.com>
2025-04-09 17:32:58 +0700

    màn setting

--- lib/presentation/view/pages/setting/setting_page.dart
+++ lib/presentation/view/pages/setting/setting_page.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_clean_architecture/gen/assets.gen.dart';
+import 'package:flutter_clean_architecture/shared/extension/context.dart';
+import 'package:gap/gap.dart';
 
 import '../../../base/base_page.dart';
 import 'setting_bloc.dart';
@@ -17,6 +20,104 @@ class SettingPage extends BasePage<SettingBloc, SettingEvent, SettingState> {
 
   @override
   Widget builder(BuildContext context) {
-    return const SizedBox();
+    final textTheme = context.themeOwn().textTheme;
+    final colorSchema = context.themeOwn().colorSchema;
+    final iconColor = Theme.of(context).iconTheme.color;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        shadowColor: Colors.transparent,
+        leading: IconButton(
+          padding: EdgeInsets.only(left: 16),
+          onPressed: context.pop,
+          icon: Assets.icons.backIcon.svg(color: iconColor),
+        ),
+        title: Text(
+          'Setting',
+          style: textTheme?.textMedium?.copyWith(color: colorSchema?.darkBlack),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Assets.icons.settingIcon1.svg(),
+                  const Gap(4),
+                  Text(
+                    'Notification',
+                    style: textTheme?.textMedium?.copyWith(
+                      color: colorSchema?.darkBlack,
+                    ),
+                  ),
+                  Spacer(),
+                  Assets.icons.settingIcon31.svg(),
+                ],
+              ),
+              const Gap(48),
+              Row(
+                children: [
+                  Assets.icons.settingIcon2.svg(),
+                  Gap(4),
+                  Text(
+                    'Security',
+                    style: textTheme?.textMedium?.copyWith(
+                      color: colorSchema?.darkBlack,
+                    ),
+                  ),
+                  Spacer(),
+                  Assets.icons.settingIcon31.svg(),
+                ],
+              ),
+              const Gap(48),
+              Row(
+                children: [
+                  Assets.icons.settingIcon3.svg(),
+                  Gap(4),
+                  Text(
+                    'Help',
+                    style: textTheme?.textMedium?.copyWith(
+                      color: colorSchema?.darkBlack,
+                    ),
+                  ),
+                  Spacer(),
+                  Assets.icons.settingIcon31.svg(),
+                ],
+              ),
+              const Gap(48),
+              Row(
+                children: [
+                  Assets.icons.settingIcon4.svg(),
+                  Gap(4),
+                  Text(
+                    'Help',
+                    style: textTheme?.textMedium?.copyWith(
+                      color: colorSchema?.darkBlack,
+                    ),
+                  ),
+                  Spacer(),
+                  Assets.icons.toggle.svg(),
+                ],
+              ),
+              const Gap(48),
+              Row(
+                children: [
+                  Assets.icons.settingIcon5.svg(),
+                  Gap(4),
+                  Text(
+                    'Logout',
+                    style: textTheme?.textMedium?.copyWith(
+                      color: colorSchema?.darkBlack,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
